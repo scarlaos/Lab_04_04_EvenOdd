eac8cd137c990b900734df2ea7ed1a9291a73b53
scarlaos <scarlaos@mail.uc.edu>
2024-09-21 15:31:35 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int numToExamineA = 5;
+        int determineEvenOdd = 2;
+        determineEvenOdd = numToExamineA % 2;
+        System.out.println("The modulo will reply with 1 if the number is odd, and 0 if the number is even. ");
+        System.out.println("The number " + numToExamineA + " comes back with " + determineEvenOdd + ", so it is odd! ");
+
+    }
+}
